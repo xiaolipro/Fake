669ccd5adf9ce2efd34e1b3a73fba2d62a4a0147
xiaolipro <2357729423@qq.com>
2023-04-28 18:07:52 +0800

    1

--- src/Fake.EntityFrameworkCore.IntegrationEventLog/Fake/EntityFrameworkCore/IntegrationEventLog/IIntegrationEventLogService.cs
+++ src/Fake.EntityFrameworkCore.IntegrationEventLog/Fake/EntityFrameworkCore/IntegrationEventLog/IIntegrationEventLogService.cs
@@ -6,10 +6,15 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Fake.EntityFrameworkCore.IntegrationEventLog;
 
-public interface IIntegrationEventLogService: IDisposable
+public interface IIntegrationEventLogService : IDisposable
 {
     Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+
+    /// <exception cref="T:System.ArgumentNullException">
+    /// <paramref name="transaction" /> is <see langword="null" />.
+    /// </exception>
     Task SaveEventAsync(IEvent integrationEvent, IDbContextTransaction transaction);
+
     Task MarkEventAsPublishedAsync(Guid eventId);
     Task MarkEventAsInProgressAsync(Guid eventId);
     Task MarkEventAsFailedAsync(Guid eventId);
