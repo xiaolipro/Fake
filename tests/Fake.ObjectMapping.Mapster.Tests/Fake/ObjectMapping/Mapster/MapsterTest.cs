d2110c3a6a37d882338d3936b36a6a313337b744
laiwang <2799199459@qq.com>
2023-07-28 17:11:54 +0800

    添加mapster 单元测试

---
+++ tests/Fake.ObjectMapping.Mapster.Tests/Fake/ObjectMapping/Mapster/MapsterTest.cs
@@ -0,0 +1,29 @@
+﻿using Fake.ObjectMapping.Models;
+using Fake.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fake.ObjectMapping.Mapster;
+
+public class MapsterTest : FakeIntegrationTest<FakeObjectMappingMapsterTestModule>
+{
+    private readonly IObjectMapper _objectMapper;
+    public MapsterTest() => _objectMapper = ServiceProvider.GetRequiredService<IObjectMapper>();
+
+    [Fact]
+    void 使用Mapster作为ObjectMapper供应商()
+    {
+        var provider = ServiceProvider.GetRequiredService<IObjectMappingProvider>();
+        Assert.True(provider is MapsterObjectMappingProvider);
+    }
+
+    [Fact]
+    void 映射()
+    {
+        var dto = _objectMapper.Map<MyEntity, MyDto>(new MyEntity { Number = 42 });
+        Assert.Equal(dto.Number, 42);
+
+        // ReverseMap
+        var entity = _objectMapper.Map<MyDto, MyEntity>(new MyDto { Number = 42 });
+        Assert.Equal(entity.Number, 42);
+    }
+}
---
+++ tests/Fake.ObjectMapping.Mapster.Tests/FakeObjectMappingMapsterTestModule.cs
@@ -0,0 +1,17 @@
+using Fake.Modularity;
+using Fake.ObjectMapping.Mapster;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+
+[DependsOn(typeof(FakeObjectMappingMasterModule))]
+public class FakeObjectMappingMapsterTestModule : FakeModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.Configure<FakeMapsterOptions>(options =>
+        {
+            options.TypeAdapterConfig.Default.PreserveReference(true);
+        });
+    }
+}
