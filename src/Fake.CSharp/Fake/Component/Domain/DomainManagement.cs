0372cd6710a327c5745d0d2df2eb9c8d32d651c7
Powers <49866401+DonPangPang@users.noreply.github.com>
2023-08-04 15:21:56 +0800

    temp

---
+++ src/Fake.CSharp/Fake/Component/Domain/DomainManagement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+
+namespace Fake.CSharp;
+
+public class DomainManagement
+{
+    private readonly ConcurrentDictionary<string , WeakReference> Cache;
+    public DomainManagement()
+    {
+        Cache = new();
+    }
+}
