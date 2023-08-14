180baf8bea50c0f85f3f2c4480be1a535c93b3cd
xiaolipro <2357729423@qq.com>
2023-08-14 11:30:41 +0800

    1

---
+++ src/Fake.Consul/Fake/Consul/LoadBalancing/AbstractConsulServiceBalancer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Consul;
+using Fake.LoadBalancing;
+using Microsoft.Extensions.Logging;
+
+namespace Fake.Consul.LoadBalancing;
+
+public abstract class AbstractConsulServiceBalancer : IServiceBalancer
+{
+    public ConcurrentDictionary<string, List<ServiceResolveResult>> ServiceNodes { get; } = new();
+
+    private readonly ILogger<AbstractConsulServiceBalancer> _logger;
+    private readonly IConsulClient _client;
+
+    public AbstractConsulServiceBalancer(ILogger<AbstractConsulServiceBalancer> logger, IConsulClient client)
+    {
+        _logger = logger;
+        _client = client;
+    }
+
+    public virtual async Task ResolutionAsync(string serviceName)
+    {
+        var nodes = await GetBalancerResolveResultsAsync(serviceName);
+        if (ServiceNodes.TryGetValue(serviceName, out var oldNodes))
+        {
+            oldNodes.Clear();
+            oldNodes.AddRange(nodes);
+        }
+        else
+        {
+            ServiceNodes.TryAdd(serviceName, nodes);
+        }
+    }
+
+    public virtual string Pick(string serviceName, bool isGrpc = false)
+    {
+        if (ServiceNodes == null || !ServiceNodes.TryGetValue(serviceName, out var nodes) || nodes.Count == 0)
+        {
+            throw new FakeException($"服务：{serviceName} 节点集合为空，无法pick");
+        }
+
+        var idx = PickIndex(serviceName);
+        if (idx >= nodes.Count || idx < 0)
+        {
+            idx = 0;
+            _logger.LogWarning("均衡服务：{ServiceName} pick的索引：{Index} 超出范围，已重置为 0", serviceName, idx);
+        }
+        var res = nodes[idx];
+        return $"{res.Host}:{(isGrpc ? res.GrpcPort : res.Port)}";
+    }
+    
+    public virtual async Task<List<ServiceResolveResult>> GetBalancerResolveResultsAsync(string serviceName)
+    {
+        var entries = await _client.Health.Service(serviceName);
+
+        var nodes = entries.Response.ToList();
+
+        OnServiceEntry(serviceName, nodes);
+
+        if (nodes.Count < 1)
+        {
+            _logger.LogWarning("未能从服务：{ServiceName} 中解析到任何实例，耗时：{RequestTimeTotalMilliseconds}ms", serviceName,
+                entries.RequestTime.TotalMilliseconds);
+        }
+        else
+        {
+            _logger.LogInformation(
+                "解析服务：{ServiceName} 成功：{Uris}，耗时：{RequestTimeTotalMilliseconds}ms", serviceName,
+                string.Join(", ", nodes), entries.RequestTime.TotalMilliseconds);
+        }
+
+        return nodes.Select(x => new ServiceResolveResult
+        {
+            Host = x.Service.Address,
+            Port = x.Service.Port,
+            GrpcPort = int.TryParse(x.Service.Meta[nameof(FakeConsulClientOptions.Weight)], out var weight) ? weight : 0
+        }).ToList();
+    }
+    
+    protected abstract int PickIndex(string serviceName);
+
+    protected virtual void OnServiceEntry(string serviceName, List<ServiceEntry> entries)
+    {
+    }
+}
--- src/Fake.Consul/Fake/Consul/LoadBalancing/ConsulRandomBalancer.cs
+++ src/Fake.Consul/Fake/Consul/LoadBalancing/ConsulRandomBalancer.cs
@@ -1,40 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Consul;
-using Fake.LoadBalancing;
+﻿using Consul;
+using Fake.Helpers;
 using Microsoft.Extensions.Logging;
 
 namespace Fake.Consul.LoadBalancing;
 
-public class ConsulRandomBalancer:RandomBalancer
+public class ConsulRandomBalancer : AbstractConsulServiceBalancer
 {
-    private readonly ILogger<ConsulRandomBalancer> _logger;
-    private readonly IConsulClient _client;
-    public ConsulRandomBalancer(ILogger<ConsulRandomBalancer> logger, IConsulClient client)
+    public ConsulRandomBalancer(ILogger<ConsulRandomBalancer> logger, IConsulClient client) : base(logger, client)
     {
-        _logger = logger;
-        _client = client;
     }
-    
-    public override async Task<List<string>> ResolutionAsync(string serviceName)
+
+    protected override int PickIndex(string serviceName)
     {
-        var entries = await _client.Health.Service(serviceName);
-
-        var addresses = entries.Response.Select(x =>x.Service.Address).ToList();
-
-        if (addresses.Count < 1)
-        {
-            _logger.LogWarning("未能从服务：{ServiceName} 中解析到任何实例，耗时：{RequestTimeTotalMilliseconds}ms", serviceName,
-                entries.RequestTime.TotalMilliseconds);
-        }
-        else
-        {
-            _logger.LogInformation(
-                "解析服务：{ServiceName} 成功：{Uris}，耗时：{RequestTimeTotalMilliseconds}ms", serviceName,
-                string.Join(", ", addresses), entries.RequestTime.TotalMilliseconds);
-        }
-
-        return addresses;
+        return RandomHelper.Next(0, ServiceNodes[serviceName].Count);
     }
 }
