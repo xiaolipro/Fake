bfc48a29999cb5cd746ef04740c29a394591883b
xiaolipro <2357729423@qq.com>
2023-08-17 17:38:38 +0800

    1

---
+++ simple/ConsulServerDemo/LogFilter.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ConsulServerDemo;
+
+public class LogFilter:IEndpointFilter
+{
+    private readonly ILogger<LogFilter> _logger;
+
+    public LogFilter(ILogger<LogFilter> logger)
+    {
+        _logger = logger;
+    }
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        _logger.LogInformation("方法开始执行");
+        var res = await next(context);
+        _logger.LogInformation("方法执行完毕");
+        return res;
+    }
+}
--- simple/ConsulServerDemo/Program.cs
+++ simple/ConsulServerDemo/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Runtime.Loader;
 using ConsulServerDemo;
 using Microsoft.Extensions.Options;
 
@@ -10,11 +12,9 @@ app.MapGet("/say", () => "Hello World!");
 
 app.MapGet("/config", () => builder.Configuration["name"]);
 
-app.MapGet("/config2", (IOptions<Student> options) => options.Value.Name + "" + options.Value.Age);
+app.MapGet("/config2", (IOptions<Student> options) => options.Value.Name + "" + options.Value.Age).AddEndpointFilter<LogFilter>();
 app.MapGet("/config3", (IOptionsSnapshot<Student> options) => options.Value.Name + "" + options.Value.Age);
-
 app.Run();
-
 
 public class Student
 {
