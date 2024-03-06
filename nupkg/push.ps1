a11e789be0e0025af8698a7d7bd5bbefe1bba618
xiaolipro <2357729423@qq.com>
2024-03-06 20:58:38 +0800

    1

--- nupkg/push.ps1
+++ nupkg/push.ps1
@@ -3,7 +3,4 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $scriptPath
 Set-Location ".\packages"
 
-Get-ChildItem -Filter *.nupkg | ForEach-Object {
-    Write-Host "dotnet nuget push $( $_.FullName )"
-    & dotnet nuget push $( $_.FullName ) --source https://baget.xiaolipro.cn/v3/index.json
-}
+dotnet nuget push *.* -s xiaolipro.cn --skip-duplicate
